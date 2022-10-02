9f907ff882bbb22e5f67009508549283acb631f0
maisanosuke <msawada@ncsu.edu>
2022-10-02 15:39:20 -0700

    first commit client files

---
+++ build/precache-manifest.f85e8cb4c6623df7e3f7d00bf698a0a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1421335165caa0dc8bcc178b761e52c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7778e88c49b5a121a95",
+    "url": "/static/css/main.33841bab.chunk.css"
+  },
+  {
+    "revision": "f5e16dd2af142079b0d8",
+    "url": "/static/js/2.e022b305.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/static/js/2.e022b305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7778e88c49b5a121a95",
+    "url": "/static/js/main.c4ff4b6c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.c4ff4b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "4b9dc077a5e83bc17478662d0144dc92",
+    "url": "/static/media/memories-Logo.4b9dc077.png"
+  },
+  {
+    "revision": "18ebdb6d48b70ab2e72ac3bf9154d8be",
+    "url": "/static/media/memories-Text.18ebdb6d.png"
+  }
+]);
